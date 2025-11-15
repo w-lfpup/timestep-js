cbdf8ae315651f97ffe1f021dc3b99d0da035991
Taylor Vann <brian.t.vann@gmail.com>
2025-11-15 15:12:50 -0800

    artifact builds on ci

--- tests/timestep.test.d.ts
+++
@@ -1,6 +0,0 @@
-declare function testIntegrationAndRender(): Promise<string[]>;
-export declare const tests: (typeof testIntegrationAndRender)[];
-export declare const options: {
-	title: string;
-};
-export {};
